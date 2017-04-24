11f686d9a306be6ac904470844f5cbac3fb9c0f2
Weiß <ga38cat@mytum.de>
2017-04-24 09:25:50 +0200

    Enabled advanced integration methods for particle tracing

--- Renderer/IntegratorParticleKernels.cu
+++ Renderer/IntegratorParticleKernels.cu
@@ -64,25 +64,52 @@ __global__ void integrateParticlesKernel()
 	int lineIndex = index / c_lineInfo.lineLengthMax;
 	float deltaTime = c_lineInfo.pCheckpoints[lineIndex].DeltaT;
 
-	//hard-coded euler
-	bool stepAccepted = advect<ADVECT_EULER, filterMode>(
-		g_texVolume1,
-		vertex.Position, vertex.Time, vertex.Velocity,
-		deltaTime,
-		world2texOffset, world2texScale,
-		c_volumeInfo.velocityScale);
+	uint step = 0;
 
-	// check if we left the current brick
-	if (!isInBrick(vertex.Position, brickBoxMin, brickBoxMax)) {
-		bool isOutOfDomain = c_volumeInfo.isOutsideOfDomain(vertex.Position);
-		if (isOutOfDomain || !findBrick(vertex.Position, brickBoxMin, brickBoxMax, world2texOffset, world2texScale)) {
-			// new brick isn't available or we went out of the domain - delete particle
-			vertex.Time = -1;
-		}
-		else {
-			// semi-HACK: update velocity from new brick (can be different to previous one because of lossy compression)
-			//            this avoids excessively small time steps at some brick boundaries
-			vertex.Velocity = c_volumeInfo.velocityScale * sampleVolume<filterMode, float4, float3>(g_texVolume1, w2t(vertex.Position));
+	float timeMax = vertex.Time + deltaTime; //this is the goal to reach
+	float difference = deltaTime / 100.0f; //resulting deltaT can be +- 1% different from the target deltaT
+	                                       //otherwise, high-order integrations take a lot of time, because
+	                                       //they execute towards the end many, many small time steps
+	bool stayedInAvailableBrick = true;
+	while (step < c_integrationParams.stepCountMax &&
+		vertex.Time + difference < timeMax
+		&& stayedInAvailableBrick)
+	{
+		float deltaTimeBak = deltaTime;
+		// limit deltaTime ..
+		// .. so we don't integrate past timeMax
+		deltaTime = min(deltaTime, timeMax - vertex.Time);
+		// .. so we don't leave the current brick's safe region
+		float distMax = c_integrationParams.brickSafeMarginWorld + distanceToBrickBorder(vertex.Position, brickBoxMin, brickBoxMax);
+		deltaTime = min(deltaTime, distMax / c_integrationParams.velocityMaxWorld);
+		// integrate
+		bool stepAccepted = advect<advectMode, filterMode>(
+			g_texVolume1,
+			vertex.Position, vertex.Time, vertex.Velocity,
+			deltaTime,
+			world2texOffset, world2texScale,
+			c_volumeInfo.velocityScale);
+		++step;
+		if (stepAccepted) {
+
+			// if we artificially limited deltaTime earlier, reset it now
+			// (if we didn't, the new deltaTime is larger than the backup anyway)
+			deltaTime = fmax(deltaTime, deltaTimeBak);
+			// check if we left the current brick
+			if (!isInBrick(vertex.Position, brickBoxMin, brickBoxMax)) {
+				bool isOutOfDomain = c_volumeInfo.isOutsideOfDomain(vertex.Position);
+				if (isOutOfDomain || !findBrick(vertex.Position, brickBoxMin, brickBoxMax, world2texOffset, world2texScale)) {
+					// new brick isn't available (or we went out of the domain) - get outta here
+					// (if we're still inside the domain, the new brick has already been requested in findBrick!)
+					stayedInAvailableBrick = false;
+					break;
+				}
+				else {
+					// semi-HACK: update velocity from new brick (can be different to previous one because of lossy compression)
+					//            this avoids excessively small time steps at some brick boundaries
+					vertex.Velocity = c_volumeInfo.velocityScale * sampleVolume<filterMode, float4, float3>(g_texVolume1, w2t(vertex.Position));
+				}
+			}
 		}
 	}
 
