52a27f080e992e18b0d6d271c68d187dde221049
Weiß <ga38cat@mytum.de>
2017-07-07 12:55:39 +0200

    Removed disabled code of compile-time-switched measures

--- Renderer/Measures.cuh
+++ Renderer/Measures.cuh
@@ -8,11 +8,6 @@
 #include "HeatCurrent.cuh"
 #include "MatrixMath.cuh"
 
-
-//compile-time switches are not needed -> turn them off
-#define USE_COMPILE_TIME_SWITCHES 0
-
-
 /******************************************************************************
 ** Helper functions
 ******************************************************************************/
@@ -56,78 +51,6 @@ __device__ float getPVA(const float3x3 &J);
 /*************************************************************************************************************************************
 ** Scalar measure (of the velocity field, its gradient and derived tensors)
 *************************************************************************************************************************************/
-
-#if USE_COMPILE_TIME_SWITCHES==1
-
-// compile time switched versions
-template <eMeasure M>
-__device__ inline float getMeasureFromRaw(const float4 vel4)
-{
-	switch(M)
-	{
-		case MEASURE_VELOCITY:
-			return length(make_float3(vel4));
-		case MEASURE_VELOCITY_Z:
-			return vel4.z;
-		case MEASURE_TEMPERATURE:
-			return vel4.w;
-		default:
-			//assert(false) ?
-			return 0.0f;
-	}
-}
-
-template <eMeasure M>
-__device__ inline float getMeasureFromHeatCurrent(const float3 heatCurrent)
-{
-	switch (M)
-	{
-	case MEASURE_HEAT_CURRENT:
-		return length(heatCurrent);
-	case MEASURE_HEAT_CURRENT_X:
-		return heatCurrent.x;
-	case MEASURE_HEAT_CURRENT_Y:
-		return heatCurrent.y;
-	case MEASURE_HEAT_CURRENT_Z:
-		return heatCurrent.z;
-	default:
-		//assert(false) ?
-		return 0.0f;
-	}
-}
-
-template <eMeasure M>
-__device__ inline float getMeasureFromJac(const float3x3& jacobian)
-{
-	switch(M)
-	{
-		case MEASURE_VORTICITY:
-			return length(getVorticity(jacobian));
-		case MEASURE_LAMBDA2:
-			return getLambda2(jacobian);
-		case MEASURE_QHUNT:
-			return getQHunt(jacobian);
-		case MEASURE_DELTACHONG:
-			return getDeltaChong(jacobian);
-		case MEASURE_ENSTROPHY_PRODUCTION:
-			return getEnstrophyProduction(jacobian);
-		case MEASURE_STRAIN_PRODUCTION:
-			return getStrainProduction(jacobian);
-		case MEASURE_SQUARE_ROTATION:
-			return getSquareRotation(jacobian);
-		case MEASURE_SQUARE_RATE_OF_STRAIN:
-			return getSquareRateOfStrain(jacobian);
-		case MEASURE_TRACE_JJT:
-			return TraceAAT(jacobian);
-		case MEASURE_PVA:
-			return getPVA(jacobian);
-		default:
-			//assert(false) ?
-			return 0.0f;
-	}
-}
-
-#endif
 
 // runtime switched versions
 __device__ inline float getMeasureFromRaw(eMeasure measure, const float4 vel4)
@@ -193,108 +116,6 @@ __device__ inline float getMeasureFromJac(eMeasure measure, const float3x3& jaco
 			return 0.0f;
 	}
 }
-
-#if USE_COMPILE_TIME_SWITCHES==1
-
-// interface function - compile time switched
-template <eMeasure M, eTextureFilterMode F, eMeasureComputeMode C>
-struct getMeasureFromVolume_Impl
-{
-    __device__ static inline float exec(texture<float4, cudaTextureType3D, cudaReadModeElementType> tex, float3 pos, float3 h);
-	// no implementation - only specializations allowed
-};
-
-template <eMeasure M, eTextureFilterMode F>
-struct getMeasureFromVolume_Impl<M, F, MEASURE_COMPUTE_PRECOMP_DISCARD>
-{
-    __device__ static inline float exec(texture<float4, cudaTextureType3D, cudaReadModeElementType> tex, float3 pos, float3 h)
-	{
-		// precomputed measure volume: just read value
-		return sampleVolume<F, float1, float>(g_texFeatureVolume, pos);
-	}
-};
-
-template <eTextureFilterMode F>
-struct getMeasureFromVolume_Impl<MEASURE_VELOCITY, F, MEASURE_COMPUTE_ONTHEFLY>
-{
-    __device__ static inline float exec(texture<float4, cudaTextureType3D, cudaReadModeElementType> tex, float3 pos, float3 h)
-	{
-		return getMeasureFromRaw<MEASURE_VELOCITY>(sampleVolume<F, float4, float4>(tex, pos));
-	}
-};
-
-template <eTextureFilterMode F>
-struct getMeasureFromVolume_Impl<MEASURE_VELOCITY_Z, F, MEASURE_COMPUTE_ONTHEFLY>
-{
-    __device__ static inline float exec(texture<float4, cudaTextureType3D, cudaReadModeElementType> tex, float3 pos, float3 h)
-	{
-		return getMeasureFromRaw<MEASURE_VELOCITY_Z>(sampleVolume<F, float4, float4>(tex, pos));
-	}
-};
-
-template <eTextureFilterMode F>
-struct getMeasureFromVolume_Impl<MEASURE_TEMPERATURE, F, MEASURE_COMPUTE_ONTHEFLY>
-{
-	__device__ static inline float exec(texture<float4, cudaTextureType3D, cudaReadModeElementType> tex, float3 pos, float3 h)
-	{
-		return getMeasureFromRaw<MEASURE_TEMPERATURE>(sampleVolume<F, float4, float4>(tex, pos));
-	}
-};
-
-template <eTextureFilterMode F>
-struct getMeasureFromVolume_Impl<MEASURE_HEAT_CURRENT, F, MEASURE_COMPUTE_ONTHEFLY>
-{
-	__device__ static inline float exec(texture<float4, cudaTextureType3D, cudaReadModeElementType> tex, float3 pos, float3 h)
-	{
-		return getMeasureFromHeatCurrent<MEASURE_HEAT_CURRENT>(getHeatCurrent<F>(tex, pos, h));
-	}
-};
-
-template <eTextureFilterMode F>
-struct getMeasureFromVolume_Impl<MEASURE_HEAT_CURRENT_X, F, MEASURE_COMPUTE_ONTHEFLY>
-{
-	__device__ static inline float exec(texture<float4, cudaTextureType3D, cudaReadModeElementType> tex, float3 pos, float3 h)
-	{
-		return getMeasureFromHeatCurrent<MEASURE_HEAT_CURRENT_X>(getHeatCurrent<F>(tex, pos, h));
-	}
-};
-
-template <eTextureFilterMode F>
-struct getMeasureFromVolume_Impl<MEASURE_HEAT_CURRENT_Y, F, MEASURE_COMPUTE_ONTHEFLY>
-{
-	__device__ static inline float exec(texture<float4, cudaTextureType3D, cudaReadModeElementType> tex, float3 pos, float3 h)
-	{
-		return getMeasureFromHeatCurrent<MEASURE_HEAT_CURRENT_Y>(getHeatCurrent<F>(tex, pos, h));
-	}
-};
-
-template <eTextureFilterMode F>
-struct getMeasureFromVolume_Impl<MEASURE_HEAT_CURRENT_Z, F, MEASURE_COMPUTE_ONTHEFLY>
-{
-	__device__ static inline float exec(texture<float4, cudaTextureType3D, cudaReadModeElementType> tex, float3 pos, float3 h)
-	{
-		return getMeasureFromHeatCurrent<MEASURE_HEAT_CURRENT_Z>(getHeatCurrent<F>(tex, pos, h));
-	}
-};
-
-template <eMeasure M, eTextureFilterMode F>
-struct getMeasureFromVolume_Impl<M, F, MEASURE_COMPUTE_ONTHEFLY>
-{
-    __device__ static inline float exec(texture<float4, cudaTextureType3D, cudaReadModeElementType> tex, float3 pos, float3 h)
-	{
-		float3x3 jacobian = getJacobian<F>(tex, pos, h);
-		return getMeasureFromJac<M>(jacobian);
-	}
-};
-
-template <eMeasure M, eTextureFilterMode F, eMeasureComputeMode C>
-__device__ inline float getMeasure(texture<float4, cudaTextureType3D, cudaReadModeElementType> tex, float3 pos, float h3, float measureScale)
-{
-    return (measureScale * getMeasureFromVolume_Impl<M,F,C>::exec(tex, pos, h));
-}
-
-
-#endif
 
 // interface function - runtime switched
 template <eMeasureSource source, eTextureFilterMode F, eMeasureComputeMode C>
